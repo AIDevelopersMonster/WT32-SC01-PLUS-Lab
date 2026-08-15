c20701fd1506342872cf7aba0c77e457f5cb528d
AlexMalachevsky <t89257409519@gmail.com>
2026-08-15 13:53:46 +0300

    add Arduino touch BSP API

--- libraries/WT32_SC01_PLUS/src/WT32_SC01_PLUS.h
+++ libraries/WT32_SC01_PLUS/src/WT32_SC01_PLUS.h
@@ -7,6 +7,7 @@ class WT32_SC01_PLUS_Display {
 public:
     bool begin();
     void fillScreen(uint16_t rgb565);
+    void fillRect(int x, int y, int w, int h, uint16_t rgb565);
     void drawTestPattern();
     int width() const { return wt32sc01plus::pins::LCD_WIDTH; }
     int height() const { return wt32sc01plus::pins::LCD_HEIGHT; }
@@ -33,6 +34,35 @@ private:
     bool ready_ = false;
 };
 
+struct WT32_SC01_PLUS_TouchPoint {
+    bool touched = false;
+    uint16_t rawX = 0;
+    uint16_t rawY = 0;
+    uint16_t x = 0;
+    uint16_t y = 0;
+    uint8_t event = 0;
+    uint8_t trackId = 0;
+};
+
+class WT32_SC01_PLUS_Touch {
+public:
+    bool begin();
+    bool read(WT32_SC01_PLUS_TouchPoint &point);
+    bool ready() const { return ready_; }
+    uint8_t chipCode() const { return chipCode_; }
+    uint8_t firmwareId() const { return firmwareId_; }
+    uint8_t focalTechId() const { return focalTechId_; }
+    int interruptLevel() const;
+
+private:
+    bool ready_ = false;
+    uint8_t chipCode_ = 0;
+    uint8_t firmwareId_ = 0;
+    uint8_t focalTechId_ = 0;
+
+    bool readReg(uint8_t reg, uint8_t *data, size_t len);
+};
+
 class WT32_SC01_PLUS_Audio {
 public:
     bool begin(uint32_t sampleRate = 44100);
@@ -53,6 +83,7 @@ public:
     bool begin();
     WT32_SC01_PLUS_Display &display() { return display_; }
     WT32_SC01_PLUS_Backlight &backlight() { return backlight_; }
+    WT32_SC01_PLUS_Touch &touch() { return touch_; }
     WT32_SC01_PLUS_Audio &audio() { return audio_; }
 
     const char *profileName() const;
@@ -61,5 +92,6 @@ public:
 private:
     WT32_SC01_PLUS_Display display_;
     WT32_SC01_PLUS_Backlight backlight_;
+    WT32_SC01_PLUS_Touch touch_;
     WT32_SC01_PLUS_Audio audio_;
 };
