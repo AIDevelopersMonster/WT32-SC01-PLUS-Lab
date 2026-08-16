8fbf1de2f653cc3d21ad053d7302162a23527bec
AlexMalachevsky <t89257409519@gmail.com>
2026-08-16 14:54:22 +0300

    add WiFi secrets template

---
+++ libraries/WT32_SC01_PLUS/examples/08_WiFiTest/wifi_secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Copy this file to wifi_secrets.h and fill in your local credentials.
+// wifi_secrets.h is ignored by Git and must never be committed.
+
+static const char *WIFI_TEST_SSID = "";
+static const char *WIFI_TEST_PASSWORD = "";
