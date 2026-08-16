6af0883187fb2a61edb721691c3ee7189428c7b0
AlexMalachevsky <t89257409519@gmail.com>
2026-08-16 13:20:54 +0300

    add factory-recovered RS485 pins

--- libraries/WT32_SC01_PLUS/src/WT32_SC01_PLUS_Pins.h
+++ libraries/WT32_SC01_PLUS/src/WT32_SC01_PLUS_Pins.h
@@ -41,7 +41,6 @@ static constexpr uint8_t TOUCH_ADDR = 0x38;
 static constexpr uint32_t TOUCH_I2C_HZ = 400000;
 
 // PHYSICALLY VALIDATED on Panlee V15 / 230208 by independent ESP-IDF SDSPI read test.
-// Arduino write-path validation is intentionally separate.
 static constexpr int SD_SCK  = 39;
 static constexpr int SD_MOSI = 40;
 static constexpr int SD_MISO = 38;
@@ -52,6 +51,13 @@ static constexpr uint32_t SD_SPI_HZ = 10000000;
 static constexpr int AUDIO_LRCK = 35;
 static constexpr int AUDIO_BCLK = 36;
 static constexpr int AUDIO_DOUT = 37;
+
+// FACTORY-FIRMWARE RECOVERED on Panlee V15 / 230208; physical Arduino validation pending.
+// Factory firmware configures UART1 at 115200 8N1 in ESP-IDF RS485 half-duplex mode.
+static constexpr int RS485_TX  = 42;
+static constexpr int RS485_RX  = 1;
+static constexpr int RS485_RTS = 2;
+static constexpr uint32_t RS485_BAUD = 115200;
 
 } // namespace pins
 } // namespace wt32sc01plus
