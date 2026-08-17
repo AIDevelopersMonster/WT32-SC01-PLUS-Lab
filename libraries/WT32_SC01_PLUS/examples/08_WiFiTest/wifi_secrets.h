0e2c1a86d153b17987ea5595be2d342ba7d6e495
AIDevelopersMonster <t89257409519@gmail.com>
2026-08-17 11:17:11 +0300

    Create wifi_secrets.h

---
+++ libraries/WT32_SC01_PLUS/examples/08_WiFiTest/wifi_secrets.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Copy this file to wifi_secrets.h and fill in your local credentials.
+// wifi_secrets.h is ignored by Git and must never be committed.
+static const char *WIFI_TEST_SSID = "TECNO CAMON 50";
+static const char *WIFI_TEST_PASSWORD = "2cxmfhfeb3ejqes";
