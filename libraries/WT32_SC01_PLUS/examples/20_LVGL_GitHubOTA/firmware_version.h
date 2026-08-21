d8bb1631973a6e0f87fd42cfaad824e201310bf4
AlexMalachevsky <t89257409519@gmail.com>
2026-08-21 06:57:46 +0300

    Add version metadata for GitHub OTA example

---
+++ libraries/WT32_SC01_PLUS/examples/20_LVGL_GitHubOTA/firmware_version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define WT32_OTA_VERSION "0.1.0"
+#define WT32_OTA_CHANNEL "stable"
+#define WT32_OTA_BOARD_ID "panlee-zx3d50ce08s-v15-usrc"
+#define WT32_OTA_ASSET_NAME "panlee-github-ota.bin"
+#define WT32_OTA_MANIFEST_NAME "panlee-github-ota.json"
